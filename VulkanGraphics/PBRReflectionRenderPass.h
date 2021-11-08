6e3f8977ddcc94c835d27cd54c4408e9e9b7fd76
asdf <dothackzero@outlook.com>
2021-11-08 09:56:54 -0500

    z

--- VulkanGraphics/PBRReflectionRenderPass.h
+++ VulkanGraphics/PBRReflectionRenderPass.h
@@ -5,7 +5,6 @@
 #include "SkyBoxRenderPipeline.h"
 #include "PBRPipeline.h"
 #include "PBRReflectionPipeline.h"
-#include "MultiViewSkyBoxPipeline.h"
 
 class PBRReflectionRenderPass : public BaseRenderPass
 {
@@ -26,7 +25,7 @@ public:
 	std::shared_ptr<RenderedCubeMapTexture> RenderedBloomTexture;
 
 	std::shared_ptr<PBRReflectionPipeline> pbrPipeline;
-	std::shared_ptr<MultiViewSkyBoxPipeline> skyboxPipeline;
+	std::shared_ptr<SkyBoxRenderPipeline> skyboxPipeline;
 
 	void RebuildSwapChain(std::shared_ptr<RenderedCubeMapTexture> IrradianceMap, std::shared_ptr<RenderedCubeMapTexture> PrefilerMap, std::shared_ptr<RenderedColorTexture> BRDFMap, std::vector<std::shared_ptr<RenderedDepthTexture>> ShadowMapTextureList);
 
